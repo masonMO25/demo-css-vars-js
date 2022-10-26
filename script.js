5125fa8b87fcd5be89edb8e9bb8008fb8e391274
masonMO25 <masonneale@gmail.com>
2022-10-25 20:02:42 -0500

    added adjustable font sizes

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const h1 = document.querySelector("h1");
 const h2 = document.querySelector(".art-title");
 
-// TODO: Add click event listeners to all of the buttons
 const buttons = document.querySelectorAll("button");
 
 function handleClick(event) {
-  // Use 'target' to get the specific button that was clicked
-  console.log(event.target.id);
+  const currentFontSize =
+    Number.parseFloat(getComputedStyle(h2).getPropertyValue("--font-size")) +
+    0.05;
+
+  // TODO: Add logic to change the text of the h2 elements
+  // If we clicked 'increase'...
+  if (event.target.id === "increase") {
+    h2.style.setProperty("--font-size", currentFontSize + 0.5 + "em");
+  } else {
+    h2.style.setProperty("--font-size", currentFontSize - 0.5 + "em");
+  }
 }
 
 buttons.forEach(function (button) {
   button.addEventListener("click", handleClick);
 });
+
+h2.style.setProperty("color", "red");
